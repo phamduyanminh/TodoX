b26115d3eb304538c42462d5e2cb9c32d059c511
Minh Pham <phamduyanminh3120@gmail.com>
2026-02-09 00:00:49 -0500

    Setup routing with HomePage and NotFound pages, add Sonner toast notifications

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,27 @@
+import {Toaster} from 'sonner';
+import {BrowserRouter, Routes, Route} from 'react-router';
+import HomePage from './pages/HomePage';
+import NotFound from './pages/NotFound';
+
 function App() {
 
   return (
     <>
-      
+      <Toaster/>
+
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path='/'
+            element={<HomePage/>}
+          />
+
+          <Route
+            path='*'
+            element={<NotFound/>}
+          />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
